3467c16b15534d372a7f2f9a75a6084dbe42cb65
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2021-06-04 00:01:38 +0900

    Patch.

--- bin/index.js
+++ bin/index.js
@@ -11,9 +11,9 @@ const preview = () => {
   shell.rm('-rf', 'node_modules/@yamat47/sample-blog-engine/src/public/images')
   shell.rm('-f', 'node_modules/@yamat47/sample-blog-engine/src/config/application.ts')
 
-  shell.cp('-rf', 'node_modules/@yamat47/sample-blog-engine/posts', 'node_modules/@yamat47/sample-blog-engine/src')
-  shell.cp('-rf', 'node_modules/@yamat47/sample-blog-engine/images', 'node_modules/@yamat47/sample-blog-engine/src/public')
-  shell.cp('-f', 'node_modules/@yamat47/sample-blog-engine/config.ts', 'node_modules/@yamat47/sample-blog-engine/src/config/application.ts')
+  shell.cp('-rf', 'posts', 'node_modules/@yamat47/sample-blog-engine/src')
+  shell.cp('-rf', 'images', 'node_modules/@yamat47/sample-blog-engine/src/public')
+  shell.cp('-f', 'config.ts', 'node_modules/@yamat47/sample-blog-engine/src/config/application.ts')
 
   shell.cd('node_modules/@yamat47/sample-blog-engine/src')
 
